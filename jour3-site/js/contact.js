614073fc1aa265b268a8c2830ab6c74000498c91
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-17 17:13:21 +0200

    amélioration

--- jour3-site/js/contact.js
+++ jour3-site/js/contact.js
@@ -28,4 +28,30 @@ $(".envoyer").click(function(e){
     }else{
         alert("veuillez compléter les deux champs");
     }
-})
+});
+// css (demain) et effet // jquery apparition / disparition zoom .... (15min et 20 min)
+
+// 1 mettre en forme le site ?? => style.css
+// 2 dans la page contact comment éviter d'avoir un p qui s'ajoute à chaque fois
+// 3 fini de remplir le formulaire vider les champs
+// 4 lorsque l'on clique sur le logo afficher / masquer le menu
+// 5 page d'accueil , lorsque vous cliquer sur une image => masquer / afficher le paragraphe qui suit 
+
+// 6 JSON parcourir dans la page d'accueil pour éviter d'avoir article1 et lorem 
+
+let articles = [
+    {
+        titre : "Premier article",
+        contenu : "fezhfoiezhoihzeoihoiz",
+        url: "http...."
+    },
+    {
+        titre : "Deuxième article",
+        contenu : "fezhfoiezhoihzeoihoiz",
+        url: "http...."
+    }
+]
+
+// boucle qui parourait le variable articles 
+
+articles[i].titre
